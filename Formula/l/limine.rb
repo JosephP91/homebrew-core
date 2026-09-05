eb39cc382005e7f7e37aa8b8693d37cc9673532f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-05 23:54:07 +0000

    limine 12.8.0

--- Formula/l/limine.rb
+++ Formula/l/limine.rb
@@ -1,8 +1,8 @@
 class Limine < Formula
   desc "Modern, secure, portable, multiprotocol bootloader and boot manager"
   homepage "https://github.com/Limine-Bootloader/Limine"
-  url "https://github.com/Limine-Bootloader/Limine/releases/download/v12.7.0/limine-12.7.0.tar.gz"
-  sha256 "9ee9f5203761f511340f1ed11baa9aa865266bb65725009dc6d2d8828ff70312"
+  url "https://github.com/Limine-Bootloader/Limine/releases/download/v12.8.0/limine-12.8.0.tar.gz"
+  sha256 "6fe2209457cb342ccf102d270ba953153138a191546c7801ed8ee9a6b2dcee4b"
   license "BSD-2-Clause"
 
   livecheck do
