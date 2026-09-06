bf7e5ecdae60a4269f17d1874840fca5d74383f6
Thierry Moisan <thierry.moisan@gmail.com>
2026-09-06 08:53:17 -0400

    forgejo-cli: migrate to openssl@4

--- Formula/f/forgejo-cli.rb
+++ Formula/f/forgejo-cli.rb
@@ -28,7 +28,7 @@ class ForgejoCli < Formula
 
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
-  depends_on "openssl@3"
+  depends_on "openssl@4"
 
   on_linux do
     depends_on "zlib-ng-compat"
