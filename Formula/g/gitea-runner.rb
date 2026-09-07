2ca38a931ab244c99a4b68da1befb0303ba9a745
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-07 02:36:23 +0000

    gitea-runner: update 3.4.0 bottle.

--- Formula/g/gitea-runner.rb
+++ Formula/g/gitea-runner.rb
@@ -6,11 +6,11 @@ class GiteaRunner < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e31bf1c74f94a5899730407b39e5c81e53464bcd6529c07b3f5565c281db9ea6"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "07173f594aeb903a67a612addd9f294ea1b8e29988eedc24488598d241879307"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ff7d45a7115b9fcd6f3dc7f3b31cc098a1448e3dbade3f2585160d623412b10c"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "365fe30886db0c39cf707a97ed0b985c3c480e9290d0cf0365ed45c6487bc926"
-    sha256 cellar: :any,                 x86_64_linux:  "c7040a49286451d814f78403d775d0456dffe040b44a08d9a249d09b573a4a0d"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e192d0317687258f0f8c959702b3b90752942b76338c8eab2d968b0646fbc7f9"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "be9904d0799cba633e5a915730c3bafa66d0c163a02736b252641b4c53c89c40"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "bd9bc3dd8013128b958062ed608351116515eaea2d11baefabed538d6ca96f33"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "9f3dca82a09ce8d00b25bb87e8c5a3976fba5d99d94e848b049fde647a191df3"
+    sha256 cellar: :any,                 x86_64_linux:  "102b5070eaa8495699ca5d72d05276aa1abbffc283ce109549be8bdaf964874b"
   end
 
   depends_on "go" => :build
