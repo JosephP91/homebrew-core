c7da55e350df3fe1aa33d27b903b67c59b5eed7b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-07 06:26:47 +0000

    network-doctor: update 1.16.2 bottle.

--- Formula/n/network-doctor.rb
+++ Formula/n/network-doctor.rb
@@ -6,11 +6,11 @@ class NetworkDoctor < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "2e774aeabe6c839386d13ad602620d475ec98167741ce55165a9d1ef5471e016"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2e774aeabe6c839386d13ad602620d475ec98167741ce55165a9d1ef5471e016"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2e774aeabe6c839386d13ad602620d475ec98167741ce55165a9d1ef5471e016"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "aaf69cf10b40ebaece9ea82bcbf8f907655181627c64000128ecb2832ec700ba"
-    sha256 cellar: :any,                 x86_64_linux:  "ba7490214ecc5be91a6277a16c4b647eb4d11509c57a897b9394cc4af14cba61"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "de4bca0ac31de2dd832d045b6230893c8effd63e027508c57cd84a3b6e0244a3"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "de4bca0ac31de2dd832d045b6230893c8effd63e027508c57cd84a3b6e0244a3"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "de4bca0ac31de2dd832d045b6230893c8effd63e027508c57cd84a3b6e0244a3"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "cb27cb944eed8e133e661242dc406865d15c3974fa5fd0968f4db44f150390a9"
+    sha256 cellar: :any,                 x86_64_linux:  "0340c0695d37403427b0bb538a9af3bc34d7c6949a43ae80d3109a34449d74e0"
   end
 
   depends_on "go" => :build
